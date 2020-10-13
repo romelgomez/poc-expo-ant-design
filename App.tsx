17fcf9270ad94f73168872d57deb81290fd9db6f
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-10-13 17:29:19 -0500

    adding ant.design

--- App.tsx
+++ App.tsx
@@ -1,14 +1,188 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import './App.css';
+import {
+  Table,
+  Modal,
+  Tag,
+  Button,
+  notification
+} from 'antd';
+
+// Services
+import { invoicesRef, updateInvoiceStatus } from './invoices.service';
+
+// Styles
+import 'antd/es/table/style/css';
+import 'antd/es/tag/style/css';
+import 'antd/es/modal/style/css';
+import 'antd/es/notification/style/css';
+
+// https://www.npmjs.com/package/consola
+import consola from 'consola';
+
+
+interface Invoice {
+  objID?: any;
+  invoice_number: number;
+  total: string;
+  currency: string;
+  invoice_date: string;
+  due_date: string;
+  vendor_name: string;
+  remittance_address: string;
+  status: string;
+}
+
+
+const { confirm } = Modal;
+
+const columns = [
+  {
+    title: 'Invoice #',
+    dataIndex: 'invoice_number'
+  },
+  {
+    title: 'Status',
+    key: 'status',
+    dataIndex: 'status',
+    render: (tag: string) => (
+      <span>
+        {
+          <Tag color={tag === 'pending' ? 'volcano' : 'green'} key={tag}>
+            {tag.toUpperCase()}
+          </Tag>
+        }
+      </span>
+    )
+  },
+  {
+    title: 'Total',
+    dataIndex: 'total'
+  },
+  {
+    title: 'Currency',
+    dataIndex: 'currency'
+  },
+  {
+    title: 'Invoice Date',
+    dataIndex: 'invoice_date'
+  },
+  {
+    title: 'Due Date',
+    dataIndex: 'due_date'
+  },
+  {
+    title: 'Vendor',
+    dataIndex: 'vendor_name'
+  },
+  {
+    title: 'Remittance Address',
+    dataIndex: 'remittance_address'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_: any, invoice: Invoice) => {
+      return (
+        <span>
+          <Button onClick={showConfirm(invoice)}>Approve</Button>
+        </span>
+      );
+    }
+  }
+];
+
+
+/**
+ * showConfirm function is a closure that return a function that open an modal to Approve the invoice.
+ */
+function showConfirm(invoice: Invoice) {
+  return () => {
+    confirm({
+      title: `Do you want to Approve the invoice #${invoice.invoice_number}?`,
+      content: 'This action can\'t be rollback',
+      onOk() {
+        return new Promise((resolve, reject) => {
+
+          updateInvoiceStatus(invoice.objID, 'Approved')
+            .then(() => {
+
+              notification.success({
+                message: 'DONE',
+                description: `The invoce #${invoice.invoice_number} was Approved!`,
+              });
+
+              resolve('');
+              
+            })
+            .catch((reason) => {
+
+              reject(reason);
+
+            });
+
+        })
+        .catch(reason => {
+          
+          consola.error(new Error(reason.toString()))
+
+        });
+      },
+      onCancel() { }
+    });
+  };
+}
+
 
 export default function App() {
+  const __invoices: Invoice[] = [];
+
+  const [invoices, setInvoices] = useState(__invoices);
+
+  // componentDidMount
+  useEffect(() => {
+
+    invoicesRef
+      .on('value', (snapshot) => {
+
+        var objects = snapshot.val();
+        var invoices: Invoice[] = [];
+
+        for (var objID in objects) {
+          if (objects.hasOwnProperty(objID)) {
+            invoices.push({
+              ...objects[objID],
+              objID: objID
+            });
+          }
+        }
+
+        setInvoices(invoices);
+      });
+
+    return () => {
+      // UNMOUNTED
+      invoicesRef.off();
+    };
+
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className='App'>
+      <h3 style={{ marginBottom: 16 }}>2ulaundry</h3>
+      <Table
+        rowKey={record => record.objID}
+        dataSource={invoices
+          .filter((invoice: Invoice) => invoice.status === 'pending')
+          .reverse()}
+        columns={columns}
+      />
+    </div>
   );
+  
+
 }
 
 const styles = StyleSheet.create({
---
+++ invoices.service.tsx
@@ -0,0 +1,30 @@
+import firebase from './firebase';
+
+/**
+ * Api doc ref: https://github.com/romelgomez/2ulaundry-functions
+ */
+const endpoint = 'https://us-central1-fire2ulaundry.cloudfunctions.net/invoices';
+
+/**
+ * Firebase invoices ref
+ */
+export const invoicesRef = firebase.database().ref('invoices');
+
+export const updateInvoiceStatus = (id: any, status: string) => {
+
+    const options = {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        body: JSON.stringify({
+            id: id,
+            status: status
+        })
+    };
+
+    return fetch(endpoint, options);
+}
+
+export default invoicesRef;
